6eba31a27396e81c175f0786111e4658e6d1d591
Neeraj-C08 <neerajchandekar@gmail.com>
2024-09-12 12:32:14 -0700

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,1 +1,21 @@
+public class Car{
+
+    private String carName;
+
+    private Double milesDriven;
+
+    private Double gallonsUsed;
+
+    public Car(){
+        carName ="";
+        milesDriven = 0.0;
+        gallonsUsed = 0.0;
+    }
+
+    
+    
+    
+    
+    
+}
 
