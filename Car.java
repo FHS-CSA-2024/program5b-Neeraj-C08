f9ea03da72b3bfca8b9a19e4c3b05a486d64f5d6
Neeraj-C08 <neerajchandekar@gmail.com>
2024-09-13 14:27:09 -0700

    Update Car.java

--- Car.java
+++ Car.java
@@ -2,20 +2,55 @@ public class Car{
 
     private String carName;
 
-    private Double milesDriven;
+    private double milesDriven;
 
-    private Double gallonsUsed;
+    private double gallonsUsed;
 
     public Car(){
         carName ="";
-        milesDriven = 0.0;
-        gallonsUsed = 0.0;
+        milesDriven = 200.7;
+        gallonsUsed = 10.4;
     }
 
     
-    
-    
-    
-    
+     // Getter
+  public String getcarName() {
+    return carName;
+  }
+  // Setter
+  public void setName(String newName) {
+    this.carName = newName;   
 }
 
+
+ // Getter
+  public double getmilesDriven() {
+    return milesDriven;
+  }
+  // Setter
+  public void setmiles(double newMiles) {
+    this.milesDriven = newMiles;   
+}
+
+
+ public double getgallonsUsed() {
+    return gallonsUsed;
+  }
+  // Setter
+  public void setgallons(double newGallons) {
+    this.gallonsUsed = newGallons;   
+}
+
+
+public double calculateAverage(){
+    
+    double mpg = ((int)(milesDriven/gallonsUsed)*10)/10.0;
+   
+    return mpg;
+}
+
+public String toString(){
+    return carName + "averaged" + calculateAverage() + "miles per gallon";
+}
+
+}
